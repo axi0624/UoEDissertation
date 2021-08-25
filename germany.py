787704dd6daf625782f09ffaaa8fd31d954bae25
Xixi <71657508+axi0624@users.noreply.github.com>
2021-08-25 22:18:17 +0800

    Add files via upload

---
+++ germany.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import gamma
+from scipy.interpolate import spline 
+from scipy.interpolate import interp1d 
+#gamma distribution shape and scale params
+exp=3
+var=3.9*3.9
+a=exp*exp/var
+b=exp/var
+r=pd.read_csv('germany.csv')
+day=r['date']
+cases=r['smoothed']
+rt=np.zeros(len(day))
+g=np.zeros(len(day))
+x=np.arange(len(day))  
+#calculating gamma pdf with Rt
+for i in range(len(r)):
+       g[i]=gamma.pdf(i+1,a,scale=1/b)
+
+for i in range(1,len(r)):
+       res=0
+       for j in range(1,i):
+            res=res+(g[j-1]*cases[i-j-1])  
+            
+       rt[i-1]=cases[i-1]/res
+       
+rt[0]=0
+
+#splines to fit the Rt curve
+x_new = np.linspace(min(x),max(x),len(day)) 
+rt_smooth = spline(x, rt, x_new)
+
+fig=plt.figure(figsize=(15,5))  
+plt.xlabel('date')
+plt.ylabel('Rt')
+
+plt.title('Rt of Germany')
+
+plt.grid(axis='y')
+plt.plot(x_new,rt_smooth,c='b') 
+plt.legend() 
+plt.show()      
+ 
+ 
+
+           
+                 
+           
+                         
